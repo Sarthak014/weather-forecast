c5a394676303503a7a5840c196b9481194d94152
Sarthak Agarwal <agarwal.sarthak001@gmail.com>
2024-03-24 15:24:04 +0530

    update env

--- src/constants/env.const.js
+++ src/constants/env.const.js
@@ -1,3 +1,3 @@
-export const API_KEY = '5ed514de8e4c71dfc2bf62dcc9cbbf05';
-export const BASE_URL = "https://api.openweathermap.org/data/";
+export const API_KEY = '1f7656826c0d5a70c17fd0aee70c0c79';
+export const BASE_URL = "https://api.openweathermap.org/data";
 export const BASE_IMG_URL = "http://openweathermap.org/img/wn/";
