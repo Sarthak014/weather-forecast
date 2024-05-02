1b3f31a9d91fe43bf9dcef6c177dacad5ab80ec3
Sarthak Agarwal <agarwal.sarthak001@gmail.com>
2024-05-02 14:34:48 +0530

    fix: added style to selected temp button

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,13 +1,15 @@
 import PropTypes from "prop-types";
 import { Fragment } from 'react';
 
-const ButtonGroup = ({ classes, data, isDivider, handleClick }) => {
+const ButtonGroup = ({ classes, data, isDivider, selected, handleClick }) => {
   return (
     <div className="flex flex-row items-center justify-center">
       {data.map(({ id, name, text }, index) => {
+        const activeBtnStyle = selected===name ? 'text-cyan-300' : 'text-white';
+
         return (
           <Fragment key={id}>
-            <button id={id} name={name} className={classes} onClick={handleClick}>
+            <button id={id} name={name} className={`${classes} ${activeBtnStyle}`} onClick={handleClick}>
               {text}
             </button>
 
